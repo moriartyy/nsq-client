beb3cf44a6ca42ea9383091b1c9bb22be3973cd9
hongmiao.yu <hongmiao.yu@mtime.com>
2018-07-24 16:26:13 +0800

    Add KeepAliveThread class

--- src/test/java/com/mtime/mq/nsq/CommandTest.java
+++ src/test/java/com/mtime/mq/nsq/CommandTest.java
@@ -1,5 +1,6 @@
 package com.mtime.mq.nsq;
 
+import com.mtime.mq.nsq.support.KeepAliveThread;
 import org.junit.Test;
 
 /**
@@ -9,6 +10,9 @@ public class CommandTest {
     @Test
     public void generateIdentificationBody() throws Exception {
         System.out.println(Command.generateIdentificationBody(new Config()));
+        KeepAliveThread.createStarted().join();
+
+        System.out.println("hello");
     }
 
 }
